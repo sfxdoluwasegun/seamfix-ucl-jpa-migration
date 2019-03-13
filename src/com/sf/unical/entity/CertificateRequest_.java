56059c7f0b0a4d63dadaefd8d81c934e4db69b09
Okpaleke Onyebuchi <ookpaleke@seamfix.com>
2019-03-13 10:26:00 +0100

    #UNICAL-368 Added new fields in certificate request and request type entities

--- src/com/sf/unical/entity/CertificateRequest_.java
+++ src/com/sf/unical/entity/CertificateRequest_.java
@@ -4,9 +4,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-03-05T12:15:17.761+0100")
+@Generated(value="Dali", date="2019-03-11T21:06:48.265+0100")
 @StaticMetamodel(CertificateRequest.class)
 public class CertificateRequest_ {
 	public static volatile SingularAttribute<CertificateRequest, Long> id;
 	public static volatile SingularAttribute<CertificateRequest, RequestHistory> requestHistory;
+	public static volatile SingularAttribute<CertificateRequest, String> graduatingYear;
+	public static volatile SingularAttribute<CertificateRequest, String> degreeClass;
 }
